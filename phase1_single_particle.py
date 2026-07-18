476ac1557bee47a44b54d586c479344dd5555a6c
abe-narayan <Abez.narayan@gmail.com>
2026-07-17 18:02:17 -0700

    progress on phase 1

--- phase1_single_particle.py
+++ phase1_single_particle.py
@@ -14,3 +14,36 @@ before we build anything on top of it.
 # TODO: check norm stays ~1 the whole way through
 # TODO: compare center-of-mass motion to the known analytic solution
 # TODO: save a plot to plots/
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+h_bar = 1.0
+m= 1.0
+omega = 1.0
+x_min = -10.0
+x_max = 10.0
+N = 1024
+
+x = np.linspace(x_min, x_max, N)
+dx = x[1] - x[0]
+
+k = 2 * np.pi * np.fft.fftfreq(N, d=dx)
+
+x_0 = 2.0
+sigma_0 = 1.0/np.sqrt(2.0)
+
+k_0 = 0.0
+psi = np.exp(-(x - x_0)**2 / (2 * sigma_0**2)) * np.exp(1j * k_0 * x)
+
+norm = np.sum(np.abs(psi)**2) * dx
+psi = psi / np.sqrt(norm)
+
+plt.figure()
+plt.plot(x, np.abs(psi)**2)
+plt.xlabel("x")
+plt.ylabel("|psi(x)|^2")
+plt.title("Initial wavepacket")
+plt.savefig("initial_wavepacket.png")
+plt.savefig("initial_wavepacket.png")
+plt.close()
