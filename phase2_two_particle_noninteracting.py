9ad0e5411df49da1236243dbc8ddfaf16653eb15
mtseng15h <mtseng15h@berkeley.edu>
2026-07-30 19:14:41 -0700

    Update phase2_two_particle_noninteracting.py

--- phase2_two_particle_noninteracting.py
+++ phase2_two_particle_noninteracting.py
@@ -6,6 +6,27 @@ nonzero here, the bug's in the entropy code, not the physics, since a
 product state literally can't entangle.
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Setting up...
+h_bar = 1.0
+m = 1.0
+omega = 1.0
+
+x = np.linspace(x_min, x_max, N, endpoint=False)
+dx = x[1] - x[0]
+
+k = 2.0 * np.pi * np.fft.fftfreq(N, d=dx)
+
+## Matrix graph thing
+
+x1, x2 = np.meshgrid(x, x, indexing="ij")
+x1[i, j] == x[i]
+x2[i, j] == x[j]
+
+
+
 # TODO: build a 2D grid for x1 and x2
 # TODO: initial state as a product: Psi(x1,x2,0) = psi1(x1) * psi2(x2)
 # TODO: potential is just V(x1) + V(x2), no interaction term
